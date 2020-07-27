e2ca209b4910ec222b69165dc582f679f3fba617
Ilya Kreymer <ikreymer@gmail.com>
2020-07-27 08:26:57 -0700

    formatting: use black code format, move package into src/ directory

--- src/warc2zim/main.py
+++ src/warc2zim/main.py
@@ -27,24 +27,24 @@ from warcio import ArchiveIterator
 from libzim.writer import Article, Blob, Creator
 
 # Shared logger
-logger = logging.getLogger('warc2zim')
+logger = logging.getLogger("warc2zim")
 
-DEFAULT_REPLAY_SOURCE_URL = 'https://cdn.jsdelivr.net/npm/replaywebpage@1.1.0-alpha.1/'
-
+DEFAULT_REPLAY_SOURCE_URL = "https://cdn.jsdelivr.net/npm/replaywebpage@1.1.0-alpha.1/"
 
 
 # ============================================================================
 class BaseArticle(Article):
     """ BaseArticle for all ZIM Articles in warc2zim with default settings
     """
+
     def is_redirect(self):
         return False
 
     def get_title(self):
-        return ''
+        return ""
 
     def get_filename(self):
-        return ''
+        return ""
 
     def should_compress(self):
         return True
@@ -57,6 +57,7 @@ class BaseArticle(Article):
 class BaseWARCArticle(BaseArticle):
     """ BaseWARCArticle that produces ZIM articles from WARC records
     """
+
     def __init__(self, record):
         super(BaseWARCArticle, self).__init__()
         self.record = record
@@ -67,22 +68,23 @@ class WARCHeadersArticle(BaseWARCArticle):
     """ WARCHeadersArticle used to store the WARC + HTTP headers as text
     Usually stored under H namespace
     """
+
     def __init__(self, record):
         super(WARCHeadersArticle, self).__init__(record)
-        self.url = record.rec_headers.get('WARC-Target-URI')
+        self.url = record.rec_headers.get("WARC-Target-URI")
 
     def get_url(self):
-        return 'H/' + self.url.split('//', 2)[1]
+        return "H/" + self.url.split("//", 2)[1]
 
     def get_mime_type(self):
-        return 'application/warc-headers'
+        return "application/warc-headers"
 
     def get_data(self):
         # add WARC headers
-        buff = self.record.rec_headers.to_bytes(encoding='utf-8')
+        buff = self.record.rec_headers.to_bytes(encoding="utf-8")
         # add HTTP headers, if present
         if self.record.http_headers:
-            buff += self.record.http_headers.to_bytes(encoding='utf-8')
+            buff += self.record.http_headers.to_bytes(encoding="utf-8")
 
         return Blob(buff)
 
@@ -92,30 +94,30 @@ class WARCPayloadArticle(BaseWARCArticle):
     """ WARCPayloadArticle used to store the WARC payload
     Usually stored under A namespace
     """
+
     def __init__(self, record):
         super(WARCPayloadArticle, self).__init__(record)
-        self.url = record.rec_headers.get('WARC-Target-URI')
+        self.url = record.rec_headers.get("WARC-Target-URI")
         self.mime = self._compute_mime()
         # TODO: converting text/html to text/unchanged-html to avoid rewriting by kiwix
         # original mime type still preserved in the headers block
         if self.mime:
-            self.mime = self.mime.split(';', 1)[0]
-            if self.mime == 'text/html':
-                self.mime = 'text/unchanged-html'
+            self.mime = self.mime.split(";", 1)[0]
+            if self.mime == "text/html":
+                self.mime = "text/unchanged-html"
         else:
-            self.mime = 'application/octet-stream'
-
+            self.mime = "application/octet-stream"
 
     def _compute_mime(self):
         if self.record.http_headers:
-        # if the record has HTTP headers, use the Content-Type from those (eg. 'response' record)
-            return self.record.http_headers['Content-Type']
+            # if the record has HTTP headers, use the Content-Type from those (eg. 'response' record)
+            return self.record.http_headers["Content-Type"]
         else:
-        # otherwise, use the Content-Type from WARC headers
-            return self.record.rec_headers['Content-Type']
+            # otherwise, use the Content-Type from WARC headers
+            return self.record.rec_headers["Content-Type"]
 
     def get_url(self):
-        return 'A/' + self.url.split('//', 2)[1]
+        return "A/" + self.url.split("//", 2)[1]
 
     def get_mime_type(self):
         return self.mime
@@ -134,14 +136,16 @@ class RWPStaticArticle(BaseArticle):
         try:
             resp = requests.get(self.prefix + filename)
             self.content = resp.content
-            self.mime = resp.headers.get('Content-Type').split(';')[0]
+            self.mime = resp.headers.get("Content-Type").split(";")[0]
         except Exception as e:
             logger.error(e)
-            logger.error('Unable to load replay system file: {0}'.format(self.prefix + filename))
+            logger.error(
+                "Unable to load replay system file: {0}".format(self.prefix + filename)
+            )
             raise
 
     def get_url(self):
-        return 'A/' + self.filename
+        return "A/" + self.filename
 
     def get_mime_type(self):
         return self.mime
@@ -158,10 +162,10 @@ class RWPViewerArticle(BaseArticle):
         self.main_url = main_url
 
     def get_url(self):
-        return 'A/' + self.filename
+        return "A/" + self.filename
 
     def get_mime_type(self):
-        return 'text/html'
+        return "text/html"
 
     def get_data(self):
         content = """
@@ -188,17 +192,19 @@ class RWPViewerArticle(BaseArticle):
      />
   </body>
 </html>
-""".format(self.main_url)
+""".format(
+            self.main_url
+        )
 
-        return Blob(content.encode('utf-8'))
+        return Blob(content.encode("utf-8"))
 
 
 # ============================================================================
 class WARC2Zim:
-    REPLAY_STATIC_FILES = ['index.html', 'ui.js', 'sw.js']
+    REPLAY_STATIC_FILES = ["index.html", "ui.js", "sw.js"]
 
     def __init__(self, args):
-        logging.basicConfig(format='[%(levelname)s] %(message)s')
+        logging.basicConfig(format="[%(levelname)s] %(message)s")
         if args.verbose:
             logger.setLevel(logging.DEBUG)
         else:
@@ -208,7 +214,7 @@ class WARC2Zim:
         self.name = args.name
         if not self.name:
             self.name, ext = os.path.splitext(args.inputs[0])
-            self.name += '.zim'
+            self.name += ".zim"
 
         self.inputs = args.inputs
         self.replay_viewer_source = args.replay_viewer_source
@@ -219,14 +225,18 @@ class WARC2Zim:
 
     def run(self):
         for filename in self.REPLAY_STATIC_FILES:
-            self.replay_articles.append(RWPStaticArticle(self.replay_viewer_source, filename))
+            self.replay_articles.append(
+                RWPStaticArticle(self.replay_viewer_source, filename)
+            )
 
-        with Creator(self.name, main_page='viewer.html', index_language='', min_chunk_size=8192) as zimcreator:
+        with Creator(
+            self.name, main_page="viewer.html", index_language="", min_chunk_size=8192
+        ) as zimcreator:
             # add replay system
             for article in self.replay_articles:
                 zimcreator.add_article(article)
 
-            zimcreator.add_article(RWPViewerArticle('viewer.html', self.main_url))
+            zimcreator.add_article(RWPViewerArticle("viewer.html", self.main_url))
 
             for warcfile in self.inputs:
                 self.process_warc(warcfile, zimcreator)
@@ -234,60 +244,80 @@ class WARC2Zim:
             # process revisits, headers only
             for url, record in self.revisits.items():
                 if url not in self.indexed_urls:
-                    logger.debug('Adding revisit {0} -> {1}'.format(url, record.rec_headers['WARC-Refers-To-Target-URI']))
+                    logger.debug(
+                        "Adding revisit {0} -> {1}".format(
+                            url, record.rec_headers["WARC-Refers-To-Target-URI"]
+                        )
+                    )
                     zimcreator.add_article(WARCHeadersArticle(record))
 
     def process_warc(self, warcfile, zimcreator):
-        with open(warcfile, 'rb') as warc_fh:
+        with open(warcfile, "rb") as warc_fh:
             for record in ArchiveIterator(warc_fh):
                 try:
-                    if record.rec_type not in ('resource', 'response', 'revisit'):
+                    if record.rec_type not in ("resource", "response", "revisit"):
                         continue
 
-                    url = record.rec_headers['WARC-Target-URI']
+                    url = record.rec_headers["WARC-Target-URI"]
                     if url in self.indexed_urls:
-                        logger.warning('Skipping duplicate {0}, already added to ZIM'.format(url))
+                        logger.warning(
+                            "Skipping duplicate {0}, already added to ZIM".format(url)
+                        )
                         continue
 
-                    if record.rec_type != 'revisit':
+                    if record.rec_type != "revisit":
                         zimcreator.add_article(WARCHeadersArticle(record))
                         zimcreator.add_article(WARCPayloadArticle(record))
                         self.indexed_urls.add(url)
 
-                    elif record.rec_headers['WARC-Refers-To-Target-URI'] != url and url not in self.revisits:
+                    elif (
+                        record.rec_headers["WARC-Refers-To-Target-URI"] != url
+                        and url not in self.revisits
+                    ):
                         self.revisits[url] = record
                         record.raw_stream = None
 
-
-                except KeyboardInterrupt:  #pragma: no cover
-                    print('Cancelling...')
+                except KeyboardInterrupt:  # pragma: no cover
+                    print("Cancelling...")
                     return 1
 
 
 # ============================================================================
 def warc2zim(args=None):
-    parser = ArgumentParser(description='Create ZIM files from WARC files')
+    parser = ArgumentParser(description="Create ZIM files from WARC files")
 
-    parser.add_argument('-V', '--version', action='version', version=get_version())
-    parser.add_argument('-v', '--verbose', action='store_true')
+    parser.add_argument("-V", "--version", action="version", version=get_version())
+    parser.add_argument("-v", "--verbose", action="store_true")
 
-    parser.add_argument('inputs', nargs='+',
-                        help='''Paths of directories and/or files to be included in
-                                the WARC file.''')
+    parser.add_argument(
+        "inputs",
+        nargs="+",
+        help="""Paths of directories and/or files to be included in
+                                the WARC file.""",
+    )
 
-    parser.add_argument('-n', '--name',
-                        help='''Base name for WARC file, appropriate extension will be
-                                added automatically.''',
-                        metavar='name')
+    parser.add_argument(
+        "-n",
+        "--name",
+        help="""Base name for WARC file, appropriate extension will be
+                                added automatically.""",
+        metavar="name",
+    )
 
-    parser.add_argument('-r', '--replay-viewer-source',
-                        help='''URL from which to load the ReplayWeb.page replay viewer from''',
-                        default=DEFAULT_REPLAY_SOURCE_URL)
+    parser.add_argument(
+        "-r",
+        "--replay-viewer-source",
+        help="""URL from which to load the ReplayWeb.page replay viewer from""",
+        default=DEFAULT_REPLAY_SOURCE_URL,
+    )
 
-    parser.add_argument('-u', '--main-url',
-                        help='''The main url that should be loaded in the viewer on init''')
+    parser.add_argument(
+        "-u",
+        "--main-url",
+        help="""The main url that should be loaded in the viewer on init""",
+    )
 
-    parser.add_argument('-o', '--overwrite', action='store_true')
+    parser.add_argument("-o", "--overwrite", action="store_true")
 
     r = parser.parse_args(args=args)
     warc2zim = WARC2Zim(r)
@@ -297,11 +327,10 @@ def warc2zim(args=None):
 # ============================================================================
 def get_version():
     import pkg_resources
-    return '%(prog)s ' + pkg_resources.get_distribution('warc2zim').version
+
+    return "%(prog)s " + pkg_resources.get_distribution("warc2zim").version
 
 
 # ============================================================================
-if __name__ == '__main__':  #pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     warc2zim()
-
-
