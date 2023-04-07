5a9ecdc425eecd7cf2ed18c67586d32874631f71
renaud gaudin <reg@rskg.org>
2023-04-07 07:25:16 +0000

    using wabac.js 2.15.5

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read(*names, **kwargs):
         return fh.read()
 
 
-REPLAY_SOURCE_URL = "https://cdn.jsdelivr.net/npm/@webrecorder/wabac@2.15.4/dist/"
+REPLAY_SOURCE_URL = "https://cdn.jsdelivr.net/npm/@webrecorder/wabac@2.15.5/dist/"
 
 
 def download_replay(name):
