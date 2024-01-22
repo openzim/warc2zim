2ebb7f4abbe4f5d56fb9c5294a15c8e9eb1949e6
benoit74 <benoit74@users.noreply.github.com>
2024-01-22 10:24:25 +0100

    Fix logger name for simplification

--- src/warc2zim/constants.py
+++ src/warc2zim/constants.py
@@ -1,8 +1,4 @@
-from pathlib import Path
-
 from zimscraperlib import getLogger
 
-LOGGER_NAME = Path(__file__).parent.name
-
 # Shared logger with default log level at this stage
-logger = getLogger(LOGGER_NAME)
+logger = getLogger("warc2zim")
