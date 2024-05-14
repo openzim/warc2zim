f76877f9dd74021406d0386f182a21b2d03d0382
benoit74 <benoit74@users.noreply.github.com>
2024-05-14 15:37:05 +0000

    Log unexpected WARC record status codes as warnings

--- src/warc2zim/utils.py
+++ src/warc2zim/utils.py
@@ -30,8 +30,8 @@ def get_record_url(record):
     return record.rec_headers["WARC-Target-URI"]
 
 
-def get_status_code(record: ArcWarcRecord) -> HTTPStatus | None:
-    """Get the HTTP status status of a given ArcWarcRecord
+def get_status_code(record: ArcWarcRecord) -> HTTPStatus | int | None:
+    """Get the HTTP status of a given ArcWarcRecord
 
     Returns HTTPStatus value or None if status code is not found / supported
     """
@@ -40,24 +40,25 @@ def get_status_code(record: ArcWarcRecord) -> HTTPStatus | None:
     else:
         status_code = record.rec_headers.get_statuscode()
 
-    if not status_code or int(status_code) == 0:
-        # null/empty http status found, ignore it (happens when content is empty indeed,
-        # e.g. 204, seems to be a WARC convention - or maybe an old bug)
+    if status_code is None or status_code.strip() == "":
+        # null / missing http status found, ignore it
         return None
 
+    status_code = int(status_code)
+
     try:
-        status_code = HTTPStatus(int(status_code))
+        status_code = HTTPStatus(status_code)
     except ValueError:
         # invalid http status found, ignore it (happens when bad http status is
-        # returned, e.g 306)
-        return None
+        # returned, e.g 0, 306)
+        return status_code
 
     return status_code
 
 
-def can_process_status_code(status_code: HTTPStatus) -> bool:
+def can_process_status_code(status_code: HTTPStatus | int | None) -> bool:
     """Return a boolean indicating if this status code is a processable redirect"""
-    return not (
+    return isinstance(status_code, HTTPStatus) and not (
         status_code.is_informational  # not supposed to exist in WARC files
         or status_code.is_client_error
         or status_code.is_server_error
@@ -84,9 +85,9 @@ def can_process_status_code(status_code: HTTPStatus) -> bool:
     )
 
 
-def status_code_is_processable_redirect(status_code: HTTPStatus) -> bool:
+def status_code_is_processable_redirect(status_code: HTTPStatus | int | None) -> bool:
     """Return a boolean indicating if this status code is processable redirect"""
-    return status_code.is_redirection and status_code in [
+    return isinstance(status_code, HTTPStatus) and status_code in [
         HTTPStatus.MOVED_PERMANENTLY,
         HTTPStatus.FOUND,
         HTTPStatus.TEMPORARY_REDIRECT,
